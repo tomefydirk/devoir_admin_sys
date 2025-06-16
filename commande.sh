1e4a747ad8d42b3d4829dfa5004c90d030274375
tomefydirk <tomefydirk@yahoo.com>
2025-06-16 19:50:56 +0300

    question 3

--- commande.sh
+++ commande.sh
@@ -4,4 +4,12 @@ chmod 640 essai
 
 #2)verification des accès
 ls -l ./essai
+pwd
 
+#3)Essayer d’accéder aux répertoires d'autres étudiants Listez les répertoires des autres : 
+ls /home/
+
+cat /home/autre_utilisateur/fichier_test
+
+
+#4)
